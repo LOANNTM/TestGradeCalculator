13ba3cbb4aa6b451044926d667ab308da391492a
master <LoanNguyen@Khoas-MacBook-Air.local>
2023-06-23 01:21:08 +0700

    Feat: Task1 1.3 & 1.4 complete

--- lastname_firstname_grade_the_exams.py
+++ lastname_firstname_grade_the_exams.py
@@ -14,8 +14,18 @@ def getInput():
 # User input file name
 file_name = getInput()
 
-# Read file
-df = pd.read_csv(file_name, sep = '\t', header=None)
+# 1.3 Print notice and 1.4 Use try/except
+try: # 1.4 Use try/except
+            # 1.2. Read file
+            df = pd.read_csv(file_name, sep = '\t', header=None)
+
+            # 1.3 In case of open file OK
+            print("Successfully opened " + file_name)
+
+except: # 1.4 Use try/except
+            # 1.3 In case of file cannot open
+            print("File cannot be found.")
+
 
 
 
